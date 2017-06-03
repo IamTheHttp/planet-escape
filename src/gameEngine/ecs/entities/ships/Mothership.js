8e406cc2f8d142113ba29d0cbee753c0bd2e06ea
Patrick Tolosa <patrick.tolosa@galacoral.com>
2017-06-03 14:12:09 +0300

    updated files

--- src/gameEngine/ecs/entities/ships/Mothership.js
+++ src/gameEngine/ecs/entities/ships/Mothership.js
@@ -1,14 +1,14 @@
-import Entity from 'gameEngine/ecs/Entity';
-import PlayerControlled from 'gameEngine/ecs/components/PlayerControlledComponent';
-import UIComponent from 'gameEngine/ecs/components/UIComponent';
-
-class Mothership {
-  constructor() {
-    let ent = new Entity();
-    ent.addComponent(new PlayerControlled());
-    ent.addComponent(new UIComponent('canvas'));
-    return ent;
-  }
-}
-
-export default Mothership;
+// import Entity from 'gameEngine/ecs/Entity';
+// import PlayerControlled from 'gameEngine/ecs/components/PlayerControlledComponent';
+// import UIComponent from 'gameEngine/ecs/components/UIComponent';
+//
+// class Mothership {
+//   constructor() {
+//     let ent = new Entity();
+//     ent.addComponent(new PlayerControlled());
+//     ent.addComponent(new UIComponent('canvas'));
+//     return ent;
+//   }
+// }
+//
+// export default Mothership;
