b32defc8488f20b0170007f4a9d34e36ce8ed513
Patrick Tolosa <patrick.tolosa@galacoral.com>
2018-04-18 12:49:01 +0300

    feature/PE-69-Create_a_minimap_instead_of_scaling_out:
     added CanvasAPI file

---
+++ src/lib/CanvasAPI.js
@@ -0,0 +1,62 @@
+class CanvasAPI {
+  constructor(ctx) {
+    this.ctx = ctx;
+    this.shapes = new Map();
+  }
+
+  remove(id) {
+    this.shapes.delete(id);
+  }
+
+  addImage({
+    id,
+    image, // the image to display
+    x, y, // pos for x,y..
+    height, width,
+    cropStartX, cropStartY, cropSizeX, cropSizeY,
+    rotation // in radians
+  }) {
+    let ctx = this.ctx;
+    this.shapes.set(id, () => {
+      ctx.save();
+      ctx.translate(x + width / 2, y + height / 2);
+      ctx.rotate(rotation);
+      ctx.drawImage(image,
+        cropStartX, cropStartY, cropSizeX, cropSizeY,
+        -width / 2, -height / 2,  // pos in canvas // at the top left of the canvas
+        width, height); // size in canvas
+      this.ctx.restore();
+    });
+  }
+
+  addCircle({id, x, y, radius}) {
+    let ctx = this.ctx;
+    this.shapes.set(id, () => {
+      ctx.beginPath();
+      ctx.moveTo(x, y);
+      ctx.arc(x, y, radius, 0, Math.PI * 2);
+      ctx.stroke();
+      ctx.closePath();
+    })
+  }
+
+  /**
+   * Method allows us to pan around the canvas
+   */
+  pan(x, y) {
+    this.ctx.setTransform(1, 0, 0, 1, x, y);
+  }
+
+  draw() {
+    this.ctx.save();
+    this.ctx.setTransform(1, 0, 0, 1, 0, 0);
+    this.ctx.clearRect(0, 0, 200, 200);
+    this.ctx.restore();
+
+    for (var shape of this.shapes.values()) {
+      shape();
+    }
+  }
+}
+
+export default CanvasAPI;
