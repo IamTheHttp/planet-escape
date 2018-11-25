dbddab70a5f0d7c6502631c7a83cde4f71ca9d99
Patrick Tolosa <patrick.tolosa@ladbrokescoral.com>
2018-11-26 00:50:25 +0530

    master:
     stuff

--- src/pageSetup.js
+++ src/pageSetup.js
@@ -16,10 +16,10 @@ let title = document.getElementsByTagName('title')[0];
 title.innerHTML = i18n.gameTitle;
 
 // Add scaling meta data
-// let meta = document.createElement('meta');
-// meta.content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0';
-// meta.name = 'viewport';
-// document.head.appendChild(meta);
+let meta = document.createElement('meta');
+meta.content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0';
+meta.name = 'viewport';
+document.head.appendChild(meta);
 
 import './pageSetup';
 import React from 'react';
