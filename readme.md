a8d2b6378f48579f0d8a1a0f7e4565d9ffcaf44c
Patrick Tolosa <patrick.tolosa@ladbrokescoral.com>
2018-12-20 23:28:51 +0200

    develop:
     updated end of game modal to fit theme better

--- readme.md
+++ readme.md
@@ -1,9 +1,16 @@
-[ ] Delegate "Selection box selection" to game-platform (this is historically still calculated in game code
+##Known Issues
+[ ] IOS Address bar swiping can cause the display to look improper
+[ ] The Enemy Planet in the most bottom right is a bit hidden behind the Menu/Help UI
 
 
-## 20/12/2018
+##ChangeLog
+### 20/12/2018
 [V] Investigate manifest.json - needs to actually be installed on the home page to make a difference
 [V] prevent swipe-up from occurring, the Menu/Back from the top needs to be moved down
 [V] Fix the Icon of the minimize/maximize, it looks bad - Changed to + and -
 [V] Added a 'Show Hints' button on the game page and removed the 'tutorials' link in the menu
 [V] Added direct tests to GenerateMap
+[V] Improved UI for 'game-ending' notification, to look like the theme
+
+##Meta
+[ ] Delegate "Selection box selection" to game-platform (this is historically still calculated in game code
--- src/ui/components/Modal/Modal.js
+++ src/ui/components/Modal/Modal.js
@@ -4,24 +4,32 @@ import './Modal.scss';
 
 class Modal extends React.Component {
   render() {
-    return (<div className="modal show endModal">
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
+    return (
+      <div className="endGame">
+        <div className="content">
+          <div className="stats pull-left">
             <span className={this.props.gameWon ? 'gameWon' : 'gameLost'}>{this.props.text}</span>
             {this.props.children}
           </div>
-          <div className="modal-body">
-            <button onClick={this.props.onClick} className="btn btn-primary restart">{i18n.restart}</button>
+          <div className="menuButtons pull-right">
+            <button
+              onClick={this.props.onClick}
+              className="btnItem restart">{i18n.restart}
+            </button>
             {
               this.props.nextLevel &&
-              <button onClick={this.props.onNextLevel} className="btn btn-primary nextLevel">{i18n.nextLevel}</button>
+              <button
+                onClick={this.props.onNextLevel}
+                className="btnItem nextLevel">{i18n.nextLevel}
+              </button>
             }
-            <button onClick={this.props.backToMainMenu} className="btn btn-primary mainMenu">{i18n.mainMenu}</button>
+            <button
+              onClick={this.props.backToMainMenu}
+              className="btnItem mainMenu">{i18n.mainMenu}
+            </button>
           </div>
         </div>
-      </div>
-    </div>);
+      </div>);
   }
 }
 
